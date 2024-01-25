8daada3a5108c86be9dc3df68f27a21e7bf50d80
techy-savant <ogbanufavour@gmail.com>
2024-01-25 09:48:51 +0100

    Configured starter files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styles from './sass/App.module.scss';
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>
+          User Guide Starter Template...
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
