5aec255184e6a1d5cd44170c9a9f4a85ca53f57c
Dev-Odior <godspoweraino@gmail.com>
2024-02-01 12:22:10 +0100

    commented on some codes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 import { Articles, Category, SubCategories } from "./sections";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ArticleContainer } from "./components";
-import { Dummy, Employee } from "./articles";
+import { Employee } from "./articles";
 
 const App = () => {
   return (
@@ -10,9 +10,16 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" Component={Home}>
-            <Route path="" Component={Category}></Route>
-            <Route path="/categories/:id" Component={SubCategories} />
+            {/* This is my main category card section  */}
+            <Route path="" Component={Category} />
+
+            {/* This is ths subcategories */}
+            <Route path=":id" Component={SubCategories} />
+
+            {/* This is for the Articles */}
             <Route path=":id/:articles" Component={Articles} />
+
+            {/* This is for the content of the article */}
             <Route path=":id/:articles/:article" Component={ArticleContainer}>
               <Route path="" Component={Employee}></Route>
             </Route>
